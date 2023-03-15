dd080c8ebfd9ba3ad6051ebc82fd1614587745ff
MarkusSchool <nokelainennmarkus@gmail.com>
2023-03-15 12:48:48 +0200

    korjattu kommenttia

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function kirjaudu() {
 
 // Käsittelee "kirjaudu ulos" -toimintoa.
 function ulos() {
-    // Asetetaan "kirjautunut" -avaimen arvoksi "ulos" LocalStorage -objektissa.
+    // Asetetaan "kirjautunut" -avaimen arvoksi "ulos" LocalStorageen.
     localStorage.setItem("kirjautunut", "ulos")
 
     // Piilotetaan käyttäjän nimen oikealla puolella oleva elementti ja kirjaudu ulos -nappi.
